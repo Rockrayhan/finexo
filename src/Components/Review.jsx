0c20957cfc2715baf9922708ce34a15ebd9fab48
Rockrayhan <khayrulalamdict@gmail.com>
2024-11-06 15:53:00 +0600

    footer

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -2,8 +2,7 @@ import React, { useRef } from "react";
 import Slider from "react-slick";
 
 const Review = () => {
-
-    let sliderRef = useRef(null);
+  let sliderRef = useRef(null);
   const next = () => {
     sliderRef.slickNext();
   };
@@ -15,68 +14,113 @@ const Review = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
-    return (
-        <div className='container my-20'>
+  return (
+    <div className="container my-20 overflow-x-hidden py-10">
+      <div>
+        <h1 className="text-center my-10">
+          {" "}
+          What says our <span className="text-primary"> Customers </span>{" "}
+        </h1>
 
-            <div>
-                <h1 className='text-center my-10'> What says our <span className='text-primary'> Customers </span> </h1>
+        <div>
+          <div className="slider-container">
+            <Slider
+              ref={(slider) => {
+                sliderRef = slider;
+              }}
+              {...settings}
+              className=" "
+            >
 
-                <div>
 
-
-                <div className="slider-container">
-      <Slider
-        ref={slider => {
-          sliderRef = slider;
-        }}
-        {...settings}
-      >
 <div className="relative client-container">
-<div key={1}>
-          <img src="images/client1.jpg" className="rounded-full absolute client-img" alt="" />
+                <div key={1}>
+                  <img
+                    src="images/client1.jpg"
+                    className="rounded-full absolute client-img"
+                    alt=""
+                  />
 
-          <div className=" review-card py-8 px-4">
-            <h3 className="text-xl font-bold"> LusDen </h3>
-            <p className="my-3 text-sm">magna aliqua, Ut </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora pariatur autem quibusdam fugit expedita. Consectetur blanditiis assumenda quidem non ipsum et architecto neque nam aliquid
-            </p>
+                  <div className=" review-card py-8 px-4">
+                    <h3 className="text-xl font-bold"> LusDen </h3>
+                    <p className="my-3 text-sm">magna aliqua, Ut </p>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Tempora pariatur autem quibusdam fugit expedita.
+                      Consectetur blanditiis assumenda quidem non ipsum et
+                      architecto neque nam aliquid
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div className="relative client-container ">
+                <div key={1}>
+                  <img
+                    src="images/client2.jpg"
+                    className="rounded-full absolute client-img"
+                    alt=""
+                  />
+
+                  <div className=" review-card py-8 px-4">
+                    <h3 className="text-xl font-bold"> Zen Court </h3>
+                    <p className="my-3 text-sm">magna aliqua, Ut </p>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Tempora pariatur autem quibusdam fugit expedita.
+                      Consectetur blanditiis assumenda quidem non ipsum et
+                      architecto neque nam aliquid
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              
+            </Slider>
+            <div className="mt-10" style={{ textAlign: "center" }}>
+              <button className="btn review-slider-btn" onClick={previous}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+                  />
+                </svg>
+              </button>
+              <button className="btn review-slider-btn" onClick={next}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
-</div>
-<div className="relative client-container">
-<div key={1}>
-          <img src="images/client2.jpg" className="rounded-full absolute client-img" alt="" />
-
-          <div className=" review-card py-8 px-4">
-            <h3 className="text-xl font-bold"> Zen Court </h3>
-            <p className="my-3 text-sm">magna aliqua, Ut </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora pariatur autem quibusdam fugit expedita. Consectetur blanditiis assumenda quidem non ipsum et architecto neque nam aliquid
-            </p>
-          </div>
-        </div>
-</div>
-       
-      </Slider>
-      <div className="mt-10" style={{ textAlign: "center" }}>
-        <button className="button" onClick={previous}>
-          Previous
-        </button>
-        <button className="button" onClick={next}>
-          Next
-        </button>
       </div>
     </div>
-                </div>
-
-            </div>
-            
-        </div>
-    );
+  );
 };
 
 export default Review;
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,11 +1,121 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>  Hello footer </h1>
+  return (
+    <div className="footer py-20">
+      <div className="container grid-cols-4">
+        <div className="col-span-1">
+<div className="flex flex-col footer-1">
+    
+
+<h1> Address </h1>
+
+<div className="flex flex-col gap-2">
+    
+<div className="flex gap-2">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span> Location </span>
+          </div>
+          <div className="flex gap-2">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span> Call </span>
+          </div>
+          <div className="flex gap-2">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-4"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+              <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+            </svg>
+            <span> Demo@gmail.com </span>
+          </div>
+
+</div>
+
+
+<div className="flex gap-4">
+
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <FaFacebookF className="" size={22} />
+      </a>
+      <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+        <FaTwitter className="" size={22} />
+      </a>
+      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+        <FaLinkedinIn className="" size={22} />
+      </a>
+      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <FaInstagram className="" size={22} />
+      </a>
+</div>
+
+</div>
         </div>
-    );
+
+
+        <div className="col-span-1"> 
+            <h1 className="mb-10"> Info </h1>
+
+            <p>
+            necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful
+            </p>
+
+             </div>
+        <div className="col-span-1 flex flex-col gap-6 ms-10"> 
+            
+            <h1> links </h1>
+
+                <a href=""> Home </a>
+                <a href=""> About </a>
+                <a href=""> Service </a>
+                <a href=""> Why Us</a>
+                <a href=""> Team</a>
+
+             </div>
+        <div className="col-span-1"> 
+            <h1>  Subscribe </h1>
+
+            <input type="text" placeholder="Enter Email" className="bg-inherit font-bold mt-10" />
+            <hr />
+
+            <button className="btn btn-primary px-20 mt-10"> Subscribe </button>
+
+            
+
+            
+             </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
